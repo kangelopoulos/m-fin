27f64e267716b2a5140fbf701432d185b16938d1
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-01-19 10:31:40 -0500

    skeleton containers

--- client/containers/HomePage.jsx
+++ client/containers/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Payment = () => {
+const HomePage = () => {
   return <h1>hi</h1>
 }
 
-export default Payment;
+export default HomePage;
--- client/containers/Uploads.jsx
+++ client/containers/Uploads.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Payment = () => {
+const Uploads = () => {
   return <h1>hi</h1>
 }
 
-export default Payment;
+export default Uploads;
