a540fc303ef410e64e297f4e144669c181aa2db4
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-01-19 17:45:28 -0500

    begin table for previous uploads

--- server/routes/uploadRouter.js
+++ server/routes/uploadRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const uploadController = require('../controllers/uploadController');
 const router = express.Router();
 
 router.get(
   '/',
+  uploadController.get,
   (req, res) => {
     return res.status(200).json(res.locals);
   }
